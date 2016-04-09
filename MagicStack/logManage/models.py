38b4c9d90dcd8ab6239b4b76d7e138f712e7ca6d
mma <majingrzl@163.com>
2016-04-09 21:58:26 +0800

    delete useless code

--- MagicStack/logManage/models.py
+++ MagicStack/logManage/models.py
@@ -20,11 +20,6 @@ class Log(models.Model):
         return self.log_path
 
 
-class Alert(models.Model):
-    msg = models.CharField(max_length=20)
-    time = models.DateTimeField(null=True)
-    is_finished = models.BigIntegerField(default=False)
-
 
 class TtyLog(models.Model):
     log = models.ForeignKey(Log)

9e703a6e9eb2b2ac36f4d5040ef6c6ee95e944da
mma <majingrzl@163.com>
2016-04-09 22:03:30 +0800

    delete useless code

--- MagicStack/permManage/models.py
+++ MagicStack/permManage/models.py
@@ -1,17 +1,7 @@
 # -*- coding:utf-8 -*-
-import datetime
-
 from django.db import models
 from assetManage.models import Asset, AssetGroup
 from userManage.models import User, UserGroup
-
-
-class PermLog(models.Model):
-    datetime = models.DateTimeField(auto_now_add=True)
-    action = models.CharField(max_length=100, null=True, blank=True, default='')
-    results = models.CharField(max_length=1000, null=True, blank=True, default='')
-    is_success = models.BooleanField(default=False)
-    is_finish = models.BooleanField(default=False)
 
 
 class PermSudo(models.Model):

1676ad2fe152dba59771e782ccfc8907672224c5
mma <majingrzl@163.com>
2016-04-10 12:08:27 +0800

    change variavle value

--- MagicStack/userManage/user_api.py
+++ MagicStack/userManage/user_api.py
@@ -161,14 +161,14 @@ def user_add_mail(user, kwargs):
     发送用户添加邮件
     """
     user_role = {'SU': u'超级管理员', 'GA': u'组管理员', 'CU': u'普通用户'}
-    mail_title = u'恭喜你的跳板机用户 %s 添加成功 Jumpserver' % user.name
+    mail_title = u'恭喜你的跳板机用户 %s 添加成功 MagicStack' % user.name
     mail_msg = u"""
     Hi, %s
         您的用户名： %s
         您的权限： %s
         您的web登录密码： %s
         您的ssh密钥文件密码： %s
-        密钥下载地址： %s/juser/key/down/?uuid=%s
+        密钥下载地址： %s/userManage/key/down/?uuid=%s
         说明： 请登陆跳板机后台下载密钥, 然后使用密钥登陆跳板机！
     """ % (user.name, user.username, user_role.get(user.role, u'普通用户'),
            kwargs.get('password'), kwargs.get('ssh_key_pwd'), URL, user.uuid)
@@ -192,7 +192,7 @@ def get_display_msg(user, password='', ssh_key_pwd='', send_mail_need=False):
         用户名：%s <br />
         密码：%s <br />
         密钥密码：%s <br />
-        密钥下载url: %s/juser/key/down/?uuid=%s <br />
+        密钥下载url: %s/userManage/key/down/?uuid=%s <br />
         该账号密码可以登陆web和跳板机。
         """ % (URL, user.username, password, ssh_key_pwd, URL, user.uuid)
     return msg

bafd98645b1c4018a4d69aef36906854c3c25eab
mma <majingrzl@163.com>
2016-05-26 15:38:07 +0800

    修改proxy编辑不成功的问题,以及密码框显示问题

--- MagicStack/proxyManage/views.py
+++ MagicStack/proxyManage/views.py
@@ -2,9 +2,10 @@
 from django.db.models import Q
 from django.shortcuts import HttpResponse
 from datetime import datetime
-from MagicStack.api import require_role, pages, my_render, ServerError, get_object
+from MagicStack.api import require_role, pages, my_render, ServerError, get_object, logger
 from models import *
 from userManage.user_api import user_operator_record
+
 
 @require_role('admin')
 def proxy_list(request):
@@ -29,8 +30,7 @@ def proxy_list(request):
 @require_role('admin')
 @user_operator_record
 def proxy_add(request, res, *args):
-    error = ''
-    msg = ''
+    error = msg = ''
     header_title, path1, path2 = '添加代理', '代理管理', '添加代理'
     res['operator'] = path2
     if request.method == 'POST':
@@ -61,36 +61,33 @@ def proxy_add(request, res, *args):
 @require_role('admin')
 @user_operator_record
 def proxy_edit(request, res, *args):
-    error = ''
-    msg = ''
+    error = msg = ''
     header_title, path1, path2 = '编辑代理', '代理管理', '编辑代理'
     res['operator'] = path2
-    if request.method == 'GET':
-        id = request.GET.get('id', '')
-        proxy = get_object(Proxy, id=id)
-    else:
-        id = int(request.POST.get('proxy_id'))
-        proxy_name = request.POST.get('proxy_name', '')
-        user_name = request.POST.get('user_name', '')
-        password = request.POST.get('user_password', '')
-        proxy_url = request.POST.get('proxy_url', '')
+    id = request.GET.get('id', '')
+    proxy = get_object(Proxy, id=id)
+    if request.method == 'POST':
+        proxy_name = request.POST.get('proxy_name')
+        user_name = request.POST.get('user_name')
+        password = request.POST.get('password')
+        proxy_url = request.POST.get('proxy_url')
         comment = request.POST.get('comment', '')
-
         try:
             if not proxy_name:
                 raise ServerError('Proxy名不能为空')
-            if Proxy.objects.filter(proxy_name=proxy_name):
-                raise ServerError('Proxy名已存在')
-            proxy = get_object(Proxy, id=id)
-            proxy.update(proxy_name=proxy_name, username=user_name, password=password,
-                         url=proxy_url, comment=comment)
-
-            msg = '编辑Proxy[%s]成功' % proxy_name
+            proxy.proxy_name = proxy_name
+            proxy.username = user_name
+            proxy.password = password
+            proxy.url = proxy_url
+            proxy.comment = comment
+            proxy.save()
+            msg = u'编辑Proxy[%s]成功' % proxy_name
             res['content'] = msg
         except ServerError, e:
             error = e
             res['flag'] = 'false'
             res['content'] = e
+            return HttpResponse('error:%s'%e)
     return my_render('proxyManage/proxy_edit.html', locals(), request)
 
 

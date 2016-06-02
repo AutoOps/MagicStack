cbdbd2b30cbaa1d6ebcb230183d69d8f1c70a3b5
mma <majingrzl@163.com>
2016-06-02 16:02:04 +0800

    修改告警管理添加告警媒介功能

--- MagicStack/emergency/views.py
+++ MagicStack/emergency/views.py
@@ -19,7 +19,7 @@ from MagicStack.api import pages, my_render, require_role, CRYPTOR, ServerError
 from userManage.user_api import user_operator_record
 from emergency.models import EmergencyType
 
-MEDIA_TYPES = [u'电子邮件', u'微信', u'短信']
+MEDIA_TYPES = {'0': u'电子邮件', '1': u'微信', '2':u'短信'}
 
 
 @require_role('user')
@@ -44,57 +44,104 @@ def media_list(request):
 def media_add(request, res, *args):
     error = msg = ''
     header_title, path1, path2 = u'添加告警媒介', u'告警管理', u'添加告警媒介'
-    media_types = MEDIA_TYPES
     res['operator'] = path2
     if request.method == 'POST':
         media_name = request.POST.get('media_name', '')
         media_type = request.POST.get('media_type', '')
-        smtp_host = request.POST.get('smtp_host', '')
-        smtp_host_port = int(request.POST.get('smtp_host_port', 587))
-        email_user = request.POST.get('email_user', '')
-        email_user_password = request.POST.get('email_user_password', '')
-        encrypt_password = CRYPTOR.encrypt(email_user_password)
-        connect_security = request.POST.getlist('connection', [])
-        status = request.POST.get('extra', '')
-        comment = request.POST.get('comment', '')
-        is_use_tls = True if '1' in connect_security else 0
-        is_use_ssl = True if '0' in connect_security else 0
-        media_status = u'启用' if status == '1' else u'禁用'
-        media_detail = "SMTP服务器:{0}    SMTP电邮:{1}".format(smtp_host, email_user)
-        try:
-            if not media_name:
-                raise ServerError('名称不能为空')
-            if EmergencyType.objects.filter(name=media_name):
-                raise ServerError('名称已存在')
-            EmergencyType.objects.create(name=media_name, type=media_type, smtp_server=smtp_host, smtp_server_port=smtp_host_port,
-                                         status=media_status, email_username=email_user, email_password=encrypt_password,
-                                         email_use_ssl=is_use_ssl, email_use_tls=is_use_tls,detail=media_detail, comment=comment)
-            msg = '添加告警媒介[%s]成功' % media_name
-            res['content'] = msg
-            return HttpResponseRedirect(reverse('media_list'))
-        except ServerError, e:
-            error = e
-            res['flag'] = False
-            res['content'] = error
+        if media_type == '0':
+            smtp_host = request.POST.get('smtp_host', '')
+            smtp_host_port = int(request.POST.get('smtp_host_port', 587))
+            email_user = request.POST.get('email_user', '')
+            email_user_password = request.POST.get('email_user_password', '')
+            encrypt_password = CRYPTOR.encrypt(email_user_password)
+            connect_security = request.POST.getlist('connection', [])
+            status = request.POST.get('extra', '')
+            comment = request.POST.get('comment', '')
+            is_use_tls = True if '1' in connect_security else 0
+            is_use_ssl = True if '0' in connect_security else 0
+            media_detail = u"SMTP服务器:{0}    SMTP电邮:{1}".format(smtp_host, email_user)
+            try:
+                if not media_name:
+                    raise ServerError(u'名称不能为空')
+                if EmergencyType.objects.filter(name=media_name):
+                    raise ServerError(u'名称已存在')
+                EmergencyType.objects.create(name=media_name, type=media_type, smtp_server=smtp_host, smtp_server_port=smtp_host_port,
+                                             status=status, email_username=email_user, email_password=encrypt_password,
+                                             email_use_ssl=is_use_ssl, email_use_tls=is_use_tls,detail=media_detail, comment=comment)
+                msg = u'添加告警媒介[%s]成功' % media_name
+                res['content'] = msg
+                return HttpResponseRedirect(reverse('media_list'))
+            except ServerError, e:
+                error = e
+                res['flag'] = False
+                res['content'] = error
 
     return my_render('emergency/media_add.html', locals(), request)
 
 
-def media_edit(request):
-    pass
+@require_role('user')
+@user_operator_record
+def media_edit(request, res):
+    error = msg = ''
+    header_title, path1, path2 = u'编辑告警媒介', u'告警管理', u'编辑告警媒介'
+    res['operator'] = path2
+    media_id = request.GET.get('id', '')
+    media_info = EmergencyType.objects.get(id=int(media_id))
+    if request.method == 'POST':
+        media_name = request.POST.get('media_name', '')
+        media_type = request.POST.get('media_type', '')
+        if media_type == '0':
+            smtp_host = request.POST.get('smtp_host', '')
+            smtp_host_port = int(request.POST.get('smtp_host_port', 587))
+            email_user = request.POST.get('email_user', '')
+            email_user_password = request.POST.get('email_user_password', '')
+            encrypt_password = CRYPTOR.encrypt(email_user_password)
+            connect_security = request.POST.getlist('connection', [])
+            status = request.POST.get('extra', '')
+            comment = request.POST.get('comment', '')
+            is_use_tls = True if '1' in connect_security else 0
+            is_use_ssl = True if '0' in connect_security else 0
+            media_detail = u"SMTP服务器:{0}    SMTP电邮:{1}".format(smtp_host, email_user)
+            try:
+                if not media_name:
+                    raise ServerError(u'名称不能为空')
+                if EmergencyType.objects.filter(name=media_name).count()> 1:
+                    raise ServerError(u'名称已存在')
+                media = EmergencyType.objects.get(id=int(media_id))
+                media.name = media_name
+                media.type = media_type
+                media.smtp_server = smtp_host
+                media.smtp_server_port = smtp_host_port
+                media.status = status
+                media.email_username = email_user
+                media.email_password = encrypt_password
+                media.email_use_ssl = is_use_ssl
+                media.email_use_tls = is_use_tls
+                media.detail = media_detail
+                media.comment = comment
+                media.save()
+                msg = u'添加告警媒介[%s]成功' % media_name
+                res['content'] = msg
+                return HttpResponseRedirect(reverse('media_list'))
+            except ServerError, e:
+                error = e
+                res['flag'] = False
+                res['content'] = error
+
+    return my_render('emergency/media_edit.html', locals(), request)
 
 
 @require_role('user')
 @user_operator_record
 def media_del(request, res):
     msg = ''
-    res['operator'] = '删除代理'
-    res['content'] = '删除代理'
+    res['operator'] = u'删除代理'
+    res['content'] = u'删除代理'
     selected_id = request.GET.get('id')
     media_ids = selected_id.split(',')
     for item in media_ids:
         media = EmergencyType.objects.get(id=int(item))
         msg += '  %s  ' % media.name
-        res['content'] += ' [%s]  ' % media.name
+        res['content'] += u' [%s]  ' % media.name
         media.delete()
-    return HttpResponse('删除[%s]成功' % msg)
+    return HttpResponse(u'删除[%s]成功' % msg)

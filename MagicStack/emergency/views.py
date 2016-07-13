85325ca003a945c7eedf2d5c01de660fc67638dc
mma <1079205598@qq.com>
2016-07-13 10:46:32 +0800

    告警事件页面实现服务器端分页

--- MagicStack/emergency/views.py
+++ MagicStack/emergency/views.py
@@ -21,6 +21,15 @@ from emergency.models import *
 from emergency.emer_api import send_email
 
 MEDIA_TYPES = {'0': u'电子邮件', '1': u'微信', '2':u'短信'}
+EMER_CONTENT = {
+        '1': u'用户变更',
+        '2': u'资产变更',
+        '3': u'应用变更',
+        '4': u'任务变更',
+        '5': u'备份变更',
+        '6': u'授权变更',
+        '7': u'代理变更'
+    }
 
 
 @require_role('user')
@@ -230,7 +239,7 @@ def media_del(request, res):
         except Exception as e:
             res['flag'] = 'flase'
             res['content']=e.message
-            msg=u"删除告警媒介失败：%s"%e.message
+            msg = u"删除告警媒介失败：%s"%e.message
     return HttpResponse(msg)
 
 
@@ -248,15 +257,7 @@ def emergency_rule(request):
 @require_role('admin')
 def emergency_edit(request):
     response = {}
-    emer_content = {
-        '1': u'用户变更',
-        '2': u'资产变更',
-        '3': u'应用变更',
-        '4': u'任务变更',
-        '5': u'备份变更',
-        '6': u'授权变更',
-        '7': u'代理变更'
-    }
+    emer_content = EMER_CONTENT
     if request.method == "GET":
         emer_id = request.GET.get('id')
         if emer_id:
@@ -317,9 +318,40 @@ def emergency_save(request, res):
 
 @require_role('admin')
 def emergency_event(request):
-    header_title, path1, path2 = u"告警事件", u'告警管理', u'告警事件'
-    emer_events = EmergencyEvent.objects.all()
-    return my_render('emergency/emer_event.html', locals(), request)
+    if request.method == 'GET':
+        header_title, path1, path2 = u"告警事件", u'告警管理', u'告警事件'
+        return my_render('emergency/emer_event.html', locals(), request)
+    else:
+        try:
+            page_length = int(request.POST.get('length', '5'))
+            total_length = EmergencyEvent.objects.all().count()
+            keyword = request.POST.get("search")
+            rest = {
+                "iTotalRecords": 0,   # 本次加载记录数量
+                "iTotalDisplayRecords": total_length,  # 总记录数量
+                "aaData": []}
+            page_start = int(request.POST.get('start', '0'))
+            page_end = page_start + page_length
+            page_data = EmergencyEvent.objects.all()[page_start:page_end]
+            rest["iTotalRecords"] = len(page_data)
+            data = []
+            emer_content = EMER_CONTENT
+            for item in page_data:
+                res = {}
+                res['id'] = item.id
+                res['emer_time'] = item.emer_time.strftime("%Y-%m-%d %H:%M:%S")
+                res['emer_event'] = emer_content.get(str(item.emer_event.content), '')
+                res['emer_user'] = item.emer_user
+                res['emer_id'] = item.id
+                res['emer_info'] = item.emer_info
+                res['emer_result'] = u'已执行' if item.emer_result else u'未执行'
+                res['emer_content_num'] = item.emer_event.content
+                data.append(res)
+            rest['aaData'] = data
+            return HttpResponse(json.dumps(rest), content_type='application/json')
+        except Exception as e:
+            logger.error(e.message)
+
 
 
 
